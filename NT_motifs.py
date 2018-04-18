6bdafaba8522027e2dab2649a6dc80a4fc38cdde
Luke Douville <luke.douville@gmail.com>
2018-04-18 10:18:37 -0700

    Started reading in NT_motifs.py

--- NT_motifs.py
+++ NT_motifs.py
@@ -1,11 +1,14 @@
-<<<<<<< HEAD
 #IC and motifs
-=======
 #!/usr/bin/python
 
 #Luke Douville's part
 #Look for motifs in the N terminus using algorithms that generate motifs.
 
-#Locate the N terminus
+#Locate the N terminus - it's at the beginning of the amino acid sequence
 #Run algorithm to find motifs
->>>>>>> 82d30391f1258e992b1309718c6698b60f4f5315
+aaFile = open("amino_acids.txt", "r")
+aaText = aaFile.read()
+aaString = ""
+for i in range(0, len(aaText)):
+    if i > aaText.find("\n"):
+        aaString += aaText[i]
