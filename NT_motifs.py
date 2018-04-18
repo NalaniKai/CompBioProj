3cf052d1413a3e7c2ea6dc529c8bd467b2aa02f6
Luke Douville <luke.douville@gmail.com>
2018-04-18 12:39:02 -0700

    Commit to save progress on NT_motifs.py

--- NT_motifs.py
+++ NT_motifs.py
@@ -8,7 +8,28 @@
 #Run algorithm to find motifs
 aaFile = open("amino_acids.txt", "r")
 aaText = aaFile.read()
-aaString = ""
-for i in range(0, len(aaText)):
-    if i > aaText.find("\n"):
-        aaString += aaText[i]
+d = {}
+aaString = ""#For some reason thinks aaString is not initialized
+#j = 0
+def isolateAminoAcidSequences():
+#    aaString = ""
+    for i in range(0, len(aaText)):
+#        if j > 0:
+#            j += 1
+        if i + 1 < aaText.find(">", i, len(aaText)):
+            aaString += aaText[i]
+#            for j in range(i, aaText.find(">", i, len(aaText))):
+#                d[i] += aaText[j]
+#        elif i == aaText.find(">"):
+#            break
+#        else:
+#            break
+#            d[i] = aaString
+#            key = i
+#            d[key] = aaString
+            d[i] = aaString
+    print("d = ", d)
+
+#print(aaString)
+#print(d)
+isolateAminoAcidSequences()
