29ac7aa06ac6096b3b5f84bbb8bfd1887880ab89
Luke Douville <luke.douville@gmail.com>
2018-04-18 17:24:07 -0700

    Isolated each amino acid sequence as part of a dictionary

--- NT_motifs.py
+++ NT_motifs.py
@@ -6,30 +6,58 @@
 
 #Locate the N terminus - it's at the beginning of the amino acid sequence
 #Run algorithm to find motifs
+#import sys
+#
+#sys.setrecursionlimit(50000)
 aaFile = open("amino_acids.txt", "r")
 aaText = aaFile.read()
+#global d
 d = {}
+#global aaString
 aaString = ""#For some reason thinks aaString is not initialized
 #j = 0
-def isolateAminoAcidSequences():
-#    aaString = ""
+def isolateAminoAcidSequences():#x, y):#aaString):
+    global aaString
+    aaString = ""
     for i in range(0, len(aaText)):
 #        if j > 0:
 #            j += 1
         if i + 1 < aaText.find(">", i, len(aaText)):
-            aaString += aaText[i]
+            aaString += aaText[i]#capture one amino acid sequence
 #            for j in range(i, aaText.find(">", i, len(aaText))):
 #                d[i] += aaText[j]
 #        elif i == aaText.find(">"):
 #            break
-#        else:
+        elif aaText[i] == "\n" and aaText[i + 1] == ">" and i < len(aaText):
+            global d
+            d[i] = aaString
+            aaString = ""
+        else:#quit capturing amino acid sequence when the end of the sequence is reached
 #            break
 #            d[i] = aaString
 #            key = i
+#            global d
+#            d[i] = aaString
 #            d[key] = aaString
-            d[i] = aaString
-    print("d = ", d)
+#            d[i] = aaString
+#            break
+            aaString = ""
+#    x = i
+#    y =
+#    isolateAminoAcidSequences(x, y)
+#    print("d = ", d)
+    for i in range(0, len(aaText)):
+        if i in d:
+            print(i, " ", end = ",")
+    print("\n\n", d[5324])
+    print("\n\n\n")
 
 #print(aaString)
+isolateAminoAcidSequences()#0, len(aaText))#aaString)#Each amino acid sequence is now in d
 #print(d)
-isolateAminoAcidSequences()
+#print("\n\n\n", d.keys())
+#print(d.items())
+#c = aaText.count("\n>")
+#for i in range(0, len(aaText)):
+#    if
+print(d)
