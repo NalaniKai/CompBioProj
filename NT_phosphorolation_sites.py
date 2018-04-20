3c41bd404c48b5f8adce6d64f0509e7c6bed5eef
NalaniKai <megan.nalani@gmail.com>
2018-04-20 10:08:02 -0700

    added functionality to find all phosphorolation sites and print them

--- NT_phosphorolation_sites.py
+++ NT_phosphorolation_sites.py
@@ -2,7 +2,6 @@
 #Nalani
 import random
 
-'''
 def findPhosSites(sequences):
     for i in range(len(sequences) + 3):
         r = random.randint(0, len(sequences)-1)
@@ -19,11 +18,45 @@ def getMotif(seq, aminoAcid):
     i = seq.index(aminoAcid)
     front = 0
     back = 0
-    while ((i-front) < 0) or ((i+back) >= len(seq))
+    while ((i-front) < 0) or ((i+back) >= len(seq)):
         front = random.randint(0,5)
         back = random.randint(0,5)
     return seq[i-front:i+back]
-'''
+
+def getNumPhosphorolationSites(sequences):
+    sAA = []
+    yAA = []
+    tAA = []
+    for s in sequences:
+        sI = 0
+        yI = 0
+        tI = 0
+        while sI != -1 and sI+1 < len(s):
+            if sI == 0:
+                sI = s.find('S', sI)
+            else:
+                sI = s.find('S', sI+1)
+            if sI != -1:
+                sAA.append(sI)
+
+        while yI != -1 and yI+1 < len(s):
+            if yI == 0:
+                yI = s.find('Y', yI)
+            else:
+                yI = s.find('Y', yI+1)
+            if yI != -1:
+                yAA.append(yI)
+
+        while tI != -1 and tI+1 < len(s):
+            if tI == 0:
+                tI = s.find('T', tI)
+            else:
+                tI = s.find('T', tI+1)
+            if tI != -1:
+                tAA.append(tI)
+    print("SAA: ", sAA)
+    print("YAA: ", yAA)
+    print("TAA: ", tAA)
 
 def isHeader(line):
     if (line[0] == '>'):
@@ -67,4 +100,5 @@ def convertFileToSequences(filename):
     return seqs, headers
 
 seqs, headers = convertFileToSequences("test_file_parse.txt")
+getNumPhosphorolationSites(seqs)
 #findPhosSites(seqs)
